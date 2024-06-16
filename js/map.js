017771a18f92ecb4b7cd07e3e6e4c45e4e5504bd
Tiago Garcia <tiago.garcia@asq.com.br>
2024-06-16 14:31:46 -0300

    feature: mapas com marcações

--- js/map.js
+++ js/map.js
@@ -1,6 +1,88 @@
 document.addEventListener("DOMContentLoaded", function () {
-  // Função para criar mapa
-  function createMap(targetId, centerCoordinates) {
+  var allStates = [
+    { name: "Acre", coordinates: [-70.532786, -8.77] },
+    { name: "Alagoas", coordinates: [-36.661471, -9.5713] },
+    { name: "Amapá", coordinates: [-51.777512, 0.902] },
+    { name: "Amazonas", coordinates: [-63.065848, -3.4168] },
+    { name: "Bahia", coordinates: [-38.5, -12.9704] },
+    { name: "Ceará", coordinates: [-38.510197, -3.71722] },
+    { name: "Distrito Federal", coordinates: [-47.9292, -15.7801] },
+    { name: "Espírito Santo", coordinates: [-40.3366, -19.19] },
+    { name: "Goiás", coordinates: [-49.267137, -16.6869] },
+    { name: "Maranhão", coordinates: [-44.301636, -2.53] },
+    { name: "Mato Grosso", coordinates: [-56.1026, -12.64] },
+    { name: "Mato Grosso do Sul", coordinates: [-54.631, -20.51] },
+    { name: "Minas Gerais", coordinates: [-44.55, -18.1] },
+    { name: "Pará", coordinates: [-52.297, -1.46] },
+    { name: "Paraíba", coordinates: [-35.37, -7.12] },
+    { name: "Paraná", coordinates: [-51.02, -25.42] },
+    { name: "Pernambuco", coordinates: [-35.7, -8.28] },
+    { name: "Piauí", coordinates: [-42.8, -8.28] },
+    { name: "Rio de Janeiro", coordinates: [-43.2, -22.91] },
+    { name: "Rio Grande do Norte", coordinates: [-35.22, -5.81] },
+    { name: "Rio Grande do Sul", coordinates: [-51.22, -30.03] },
+    { name: "Rondônia", coordinates: [-63.9, -11.22] },
+    { name: "Roraima", coordinates: [-60.66, 2.82] },
+    { name: "Santa Catarina", coordinates: [-49.55, -27.59] },
+    { name: "São Paulo", coordinates: [-46.63, -23.55] },
+    { name: "Sergipe", coordinates: [-37.07, -10.91] },
+    { name: "Tocantins", coordinates: [-48.36, -10.25] },
+  ];
+
+  function filterStatesByRegionsAndNames(regions, names) {
+    const regionStates = {
+      Norte: [
+        "Acre",
+        "Amapá",
+        "Amazonas",
+        "Pará",
+        "Rondônia",
+        "Roraima",
+        "Tocantins",
+      ],
+      Nordeste: [
+        "Alagoas",
+        "Bahia",
+        "Ceará",
+        "Maranhão",
+        "Paraíba",
+        "Pernambuco",
+        "Piauí",
+        "Rio Grande do Norte",
+        "Sergipe",
+      ],
+      "Centro-Oeste": [
+        "Distrito Federal",
+        "Goiás",
+        "Mato Grosso",
+        "Mato Grosso do Sul",
+      ],
+      Sudeste: [
+        "Espírito Santo",
+        "Minas Gerais",
+        "Rio de Janeiro",
+        "São Paulo",
+      ],
+      Sul: ["Paraná", "Rio Grande do Sul", "Santa Catarina"],
+    };
+
+    let filteredStates = [];
+
+    regions.forEach((region) => {
+      filteredStates = filteredStates.concat(
+        allStates.filter((state) => regionStates[region].includes(state.name))
+      );
+    });
+
+    names.forEach((name) => {
+      let state = allStates.find((state) => state.name === name);
+      if (state) filteredStates.push(state);
+    });
+
+    return filteredStates;
+  }
+
+  function createMap(targetId, centerCoordinates, states) {
     var map = new ol.Map({
       target: targetId,
       layers: [
@@ -13,36 +95,6 @@ document.addEventListener("DOMContentLoaded", function () {
         zoom: 4,
       }),
     });
-
-    var states = [
-      { name: "Acre", coordinates: [-70.532786, -8.77] },
-      { name: "Alagoas", coordinates: [-36.661471, -9.5713] },
-      { name: "Amapá", coordinates: [-51.777512, 0.902] },
-      { name: "Amazonas", coordinates: [-63.065848, -3.4168] },
-      { name: "Bahia", coordinates: [-38.5, -12.9704] },
-      { name: "Ceará", coordinates: [-38.510197, -3.71722] },
-      { name: "Distrito Federal", coordinates: [-47.9292, -15.7801] },
-      { name: "Espírito Santo", coordinates: [-40.3366, -19.19] },
-      { name: "Goiás", coordinates: [-49.267137, -16.6869] },
-      { name: "Maranhão", coordinates: [-44.301636, -2.53] },
-      { name: "Mato Grosso", coordinates: [-56.1026, -12.64] },
-      { name: "Mato Grosso do Sul", coordinates: [-54.631, -20.51] },
-      { name: "Minas Gerais", coordinates: [-44.55, -18.1] },
-      { name: "Pará", coordinates: [-52.297, -1.46] },
-      { name: "Paraíba", coordinates: [-35.37, -7.12] },
-      { name: "Paraná", coordinates: [-51.02, -25.42] },
-      { name: "Pernambuco", coordinates: [-35.7, -8.28] },
-      { name: "Piauí", coordinates: [-42.8, -8.28] },
-      { name: "Rio de Janeiro", coordinates: [-43.2, -22.91] },
-      { name: "Rio Grande do Norte", coordinates: [-35.22, -5.81] },
-      { name: "Rio Grande do Sul", coordinates: [-51.22, -30.03] },
-      { name: "Rondônia", coordinates: [-63.9, -11.22] },
-      { name: "Roraima", coordinates: [-60.66, 2.82] },
-      { name: "Santa Catarina", coordinates: [-48.55, -27.59] },
-      { name: "São Paulo", coordinates: [-46.63, -23.55] },
-      { name: "Sergipe", coordinates: [-37.07, -10.91] },
-      { name: "Tocantins", coordinates: [-48.36, -10.25] },
-    ];
 
     var iconStyle = new ol.style.Style({
       image: new ol.style.Icon({
@@ -70,10 +122,44 @@ document.addEventListener("DOMContentLoaded", function () {
 
       map.addLayer(markerVectorLayer);
     });
+
+    return map;
   }
 
-  // Inicializando mapas com IDs diferentes
-  createMap("map-admin", [-48.55, -27.59]); // Centro da Hubcare Administradora
-  createMap("map-gestao", [-47.9292, -15.7801]); // Centro da Hubcare Gestão Saúde
-  createMap("map-digital", [-48.55, -27.59]); // Centro da Hubcare Digital
+  var brasilCentralizado = [-47.9292, -15.7801];
+  var mapAdmin = createMap(
+    "map-admin",
+    brasilCentralizado,
+    filterStatesByRegionsAndNames(["Nordeste"], ["Santa Catarina", "São Paulo"])
+  );
+  var mapGestao = createMap(
+    "map-gestao",
+    brasilCentralizado,
+    filterStatesByRegionsAndNames(
+      ["Sul", "Sudeste", "Nordeste", "Centro-Oeste"],
+      []
+    )
+  );
+  var mapDigital = createMap("map-digital", brasilCentralizado, allStates);
+
+  function handleResize() {
+    var maps = [mapAdmin, mapGestao, mapDigital];
+    var disableInteractions = window.innerWidth <= 767;
+
+    maps.forEach(function (map) {
+      map.getInteractions().forEach(function (interaction) {
+        if (
+          interaction instanceof ol.interaction.DragPan ||
+          interaction instanceof ol.interaction.MouseWheelZoom ||
+          interaction instanceof ol.interaction.DoubleClickZoom ||
+          interaction instanceof ol.interaction.PinchZoom
+        ) {
+          interaction.setActive(!disableInteractions);
+        }
+      });
+    });
+  }
+
+  window.addEventListener("resize", handleResize);
+  handleResize();
 });
